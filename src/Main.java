68dfe1d3fbb1fb856e34f452b1a6e80c665b4c60
JustZaza <zandemnothoza@gmail.com>
2025-04-22 23:32:39 +0200

    OTP generator method done only.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        otp_gene();
+    }
+
+    public static void otp_gene(){
+        String number = "0123456789";
+        Random rand = new Random();
+        char[] otp = new char[4];
+        for (int i = 0; i < 4; i++) {
+            otp[i] = number.charAt(rand.nextInt(number.length()));
+        }
+        System.out.println(otp);
+    }
+
+//    public static void otp_auth(){
+//        int entered = 0;
+//
+//    }
+
+}
